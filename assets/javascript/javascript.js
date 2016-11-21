0fe8d3b966d5af8d2d4869831dfb0a9e30163ba7
FH <FH@Fionas-MacBook-Air.local>
2016-11-20 18:34:54 -0600

    added logic for first selection - need to add logic for win,lose or draw, capture score  and reset for next selection

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,11 +15,14 @@ $('document').ready(function(){
 	var playerDbObj = database.child("players");
 	// var player1DbObj = playerDbObj.child("1");
 	// var player2DbObj = playerDbObj.child("2");
+	var turnObj = database.child("turn");
 	var chatDbObj	= database.child("chat");
 	var currentPlayer = null;
 	
 	var newPlayerPosition = 0;
 	var turn = 1;
+	var options = ['Rock', 'Paper', 'Scissors'];
+	
 
 	var playerData = 
 		{ 1 : { userid: "",
@@ -27,23 +30,90 @@ $('document').ready(function(){
 		losses: 0,
 		draws: 0,
 		lastChat: "",
+		guess: "",
 		lastGuess: "", 
-		isActive: true,
+		// options: ['Rock', 'Paper', 'Scissors'],
+		options: [],
 		timestamp: firebase.database.ServerValue.TIMESTAMP}, 
 		2: { userid: "",
 		wins: 0,
 		losses: 0,
 		draws: 0,
 		lastChat: "",
+		guess: "",
 		lastGuess: "", 
-		isActive: true,
+		// options: ['Rock', 'Paper', 'Scissors'],
+		options: [],
 		timestamp: firebase.database.ServerValue.TIMESTAMP}};
 	
 	var objLength = Object.keys(playerData).length;
 
+	// set what happens on disconnect - need to reset the position
+	var positionRef = database.child("playerPosition");
+
+	function selectOption(player, guess){
+        // console.log("in options function");
+		// var player = $(this).data("player");
+		// console.log(this);
+		// if it is the current players turn then allow them to select
+		if (player === turn){
+			// capture the value and player data
+			// var player = $(this).data("player");
+			// store last guess 
+			playerData[player].lastGuess = playerData[player].guess;
+			// capture current guess
+			playerData[player].guess = guess;
+			playerDbObj.child(player).child('guess').set(guess);
+			// $('.hide-initially-player' + player +' .gameplay').hide();
+			$('.player' + player + "-options").hide();
+			
+			$('#player' + player + "-choice").show();
+			
+	            
+            database.child("players").child(player).child('options').child('0').set("");
+            database.child("players").child(player).child('options').child('1').set("");
+            database.child("players").child(player).child('options').child('2').set("");
+	           
+			
+	            
+			// set turns
+			if (turn === 2){
+				// check who won
+				if (playerData[turn].guess === playerData[turn-1].guess){
+					//draw
+					console.log("draw");
+				} 
+				else if (((playerData[turn].guess === 'rock') && (playerData[turn-1].guess === 'scissors' )) || ((playerData[turn].guess === 'scissors') && (playerData[turn-1].guess === 'paper' ))|| ((playerData[turn].guess === 'paper') && (playerData[turn-1].guess === 'rock' ))) {
+					// palyer 2 wins
+					console.log("player 2 wins");
+				}
+				else if (((playerData[turn-1].guess === 'rock') && (playerData[turn].guess === 'scissors' )) || ((playerData[turn-1].guess === 'scissors') && (playerData[turn].guess === 'paper' ))|| ((playerData[turn-1].guess === 'paper') && (playerData[turn].guess === 'rock' ))) {
+					// palyer 1 wins
+					console.log("player 1 wins");
+				}
+
+				turn = 1;
+				turnObj.set(turn);
+			} 
+			else {
+				turn = 2;
+				turnObj.set(turn);
+			}
+		}
+	}
+
 	// set inintial values
-	// database.child('turn').set(turn);
 	database.child('playerPosition').set(newPlayerPosition);
+	turnObj.set(turn);
+
+	if (playerData[1].userid === "")
+	{
+		positionRef.onDisconnect().set(0);
+	}
+    else if (playerData[2].userid === "")
+	{
+		positionRef.onDisconnect().set(1);
+	}    
 
 	database.on("value", function(snapshot) {
 		console.log("in here value" + k);
@@ -60,6 +130,11 @@ $('document').ready(function(){
 				$('#player' + i + '-wins').html(snapshot.child("players").child(i).val().wins);
 				$('#player' + i + '-losses').html(snapshot.child("players").child(i).val().losses);
 				$('#player' + i + '-draws').html(snapshot.child("players").child(i).val().draws);
+				$('#player' + i + '-rock').html(snapshot.child("players").child(i).val().options[0]);
+				$('#player' + i + '-paper').html(snapshot.child("players").child(i).val().options[1]);
+				$('#player' + i + '-scissors').html(snapshot.child("players").child(i).val().options[2]);
+				$('#player' + i + '-choice').html(snapshot.child("players").child(i).val().guess);
+				
 				// newPlayerPosition = i;
 				console.log("changed to " + i);
 				turn = snapshot.val().turn;
@@ -77,6 +152,11 @@ $('document').ready(function(){
 
 			}
 			newPlayerPosition = snapshot.child("playerPosition").val();
+			// if there are 2 players then display turn information
+			if (newPlayerPosition ===  2){
+				$('#turn').removeClass('hide');
+				$('#turn').html("It is " + playerData[turn].userid + "'s turn to choose.")
+			}
 		}
 	});
 
@@ -92,48 +172,9 @@ $('document').ready(function(){
 					}
 				
 				}
-	})
+	});
 
-	// database.on("child_added", function(snapshot){
-	// 	console.log("what happenenidn");
-	// 	for (var i=1; i <= objLength; i++){
-			
-	// 		if (snapshot.child("players").child(i).exists()) {
-				
-	// 			$('.hide-initially-player' + i).show();
-	// 			$('#chat-history-div').removeClass('hide');
-	// 			$('#chat-submit-div').removeClass('hide');
-	// 			$('.show-initially-player' + i).hide();
-	// 			$('#player' + i + '-name').html(snapshot.child("players").child(i).val().userid);
-	// 			$('#player' + i + '-wins').html(snapshot.child("players").child(i).val().wins);
-	// 			$('#player' + i + '-losses').html(snapshot.child("players").child(i).val().losses);
-	// 			$('#player' + i + '-draws').html(snapshot.child("players").child(i).val().draws);
-				
-	// 			console.log("what happens here" + newPlayerPosition );
-				
-	// 			turn = snapshot.val().turn;
-	// 			console.log("to here" );
-	// 			playerData[i].userid = snapshot.child("players").child(i).val().userid;
-	// 			playerData[i].wins = snapshot.child("players").child(i).val().wins;
-	// 			playerData[i].losses = snapshot.child("players").child(i).val().losses;
-	// 			playerData[i].draws = snapshot.child("players").child(i).val().draws;
-				
-	// 		}
-	// 		else{
-	// 			$('.hide-initially-player' + i).hide();
-	// 			$('.show-initially-player' + i).show();
-				
-
-	// 		}
-
-	// 	}
-	// })
-
-
-
-
-	
-	$('#player-name-submit').on("click", function(){
+	$('#player-name-submit').on('click', function(){
 		// **** prevent "" being entered.
 		newPlayerPosition++;
 		
@@ -146,6 +187,8 @@ $('document').ready(function(){
 			$('#chat-submit-div').removeClass('hide');
 			$('.show-initially-player' + newPlayerPosition).hide();
 			$('.show-initially-player').addClass('hide');
+			playerData[newPlayerPosition].options = options;
+
 		
 			// database.child('turn').set(turn);
 			database.child('playerPosition').set(newPlayerPosition);
@@ -158,12 +201,18 @@ $('document').ready(function(){
 			var presenceRef = playerDbObj.child(newPlayerPosition);
 			// // Write a string when this client loses connection
 			presenceRef.onDisconnect().remove();
+
+			for (var j = 0; j < 3; j++){
+				var id = ('#player' + newPlayerPosition + '-' + playerData[newPlayerPosition].options[j]).toLowerCase();
+				console.log(id);
+				$(id).show();
+				console.log(playerData[newPlayerPosition].options[j]);
+				$(id).html(playerData[newPlayerPosition].options[j]);
+			}
    
 		
 		}
-
-
-	})
+	});
 
 	$('#submit-chat').on('click', function(){
 		var chatInput = currentPlayer + ' said: ' + $('#chat-input').val().trim();
@@ -172,30 +221,21 @@ $('document').ready(function(){
     	chatDbObj.push(chatInput);
 
     	return false;
-	})
-
-	// set what happens on discconnect - need to reset the position
-	var positionRef = database.child("playerPosition");
-
-	if (playerData[1].userid === "")
-	{
-		positionRef.onDisconnect().set(0);
-	}
-    else if (playerData[2].userid === "")
-	{
-		positionRef.onDisconnect().set(1);
-	}    
-
+	});
 		
-	$('.gameplay').on('click', function(){
-		console.log($(this).value);
-		// set turns
-		// validate that there are tow users before play can begin
-		// track last guess
-		// see who won and set the scores
-		// track scores
-		
-	})
+	$('.player1-options').on('click', function(){
+		var player = $(this).data("player");
+		var guess = $(this).data("guess");
+		selectOption(player, guess);
+
+	});
+
+	$('.player2-options').on('click', function(){
+		var player = $(this).data("player");
+		var guess = $(this).data("guess");
+		selectOption(player, guess);
+
+	});	
 
 })
 
